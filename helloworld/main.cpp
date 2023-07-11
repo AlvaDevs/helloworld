4f385462291f7694d1e3d86220de89ecda3e5cff
AlvaNeedsFood <aalvarez.contact05@gmail.com>
2023-07-11 18:55:05 -0500

    Initial Commit

---
+++ helloworld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Hope Alvarez on 11/07/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
