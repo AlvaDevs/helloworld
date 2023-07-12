222aa6abb38d4f4e8db784fda4589133dfe88ea9
AlvaNeedsFood <aalvarez.contact05@gmail.com>
2023-07-12 00:45:14 -0500

    First change

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -2,13 +2,16 @@
 //  main.cpp
 //  helloworld
 //
-//  Created by Hope Alvarez on 11/07/23.
+//  Created by Alvaro Alvarez on 11/07/23.
 //
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    
+    cout << "Hello, World!\n";
+    
     return 0;
 }
